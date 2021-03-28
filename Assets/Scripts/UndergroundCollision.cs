dc0b7d430d2b3f8e0f59e6e35a6a93e0c24ee176
ManStr1 <190117006@stu.sdu.edu.kz>
2021-03-28 21:47:19 +0600

    Added prefabs and created death zone

---
+++ Assets/Scripts/UndergroundCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UndergroundCollision : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        string tag = other.tag;
+
+        if (tag.Equals("Food")) {
+            Destroy(other.gameObject);
+            Debug.Log("food");
+        }
+        if (tag.Equals("Item")) {
+            Destroy(other.gameObject);
+            Debug.Log("item");
+        }
+    }
+}
